e3ccd3b7adf575e6a159f0e11b3d9e1264de9214
bharatknv <40297858+bharatknv@users.noreply.github.com>
2024-01-05 20:50:45 +0530

    Add custom keyboard layout

---
+++ addons/languages/english_custom/pack/src/main/java/com/anysoftkeyboard/languagepack/english_custom/PackBroadcastReceiver.java
@@ -0,0 +1,5 @@
+package com.anysoftkeyboard.languagepack.english_custom;
+
+import com.anysoftkeyboard.addon.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {}
